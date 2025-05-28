adf59ca586471b798981d89595e3486dd01eb6e6
Academy <aper29012006@gmail.com>
2025-05-28 20:40:39 +0300

    Added redirection to user pages

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    pass
+
+class RegistrationView(CreateView):
+    pass
+
+class AccountView(DetailView):
+    pass
+
+class UserLogoutView(LogoutView):
+    pass
