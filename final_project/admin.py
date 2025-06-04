454d27e8c5af13e0ef44425f8bac47b75368e9be
frol1k3 <alex_frolov24042007@mail.ru>
2025-06-04 21:01:19 +0300

    сделал католог, на котором отбражаются все вакансии и оформил стриницу с информацией о нас

--- final_project/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from user.models import *
-from vacancy.models import *
-
-admin.site.register(Profile)
-admin.site.register(Vacancy)
--- vacancy/views.py
+++ vacancy/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from vacancy.models import *
+from django.views.generic import ListView
+from django.http import HttpRequest, JsonResponse
+from django.urls import reverse
 
+class IndexListView (ListView):
+    model = Vacancy
+    template_name = "catalog.html"
+    context_object_name = "vacancies"
 
 def catalog(req):
-    return render(req, "catalog.html")
-# Create your views here.
+    vacancies = Vacancy.objects.all()
+    return render(req, "catalog.html", {"vacancies": vacancies})
+
+
+
