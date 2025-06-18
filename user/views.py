b70c8a13dd007568f9cb6a2a6469b7507e7ec24e
Academy <aper29012006@gmail.com>
2025-06-18 19:03:56 +0300

    Authorization, user authentication. Personal account. V3.2

--- user/views.py
+++ user/views.py
@@ -2,10 +2,11 @@ from django.views.generic import CreateView, DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.urls import reverse
 from user.forms import *
 from user.models import *
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpRequest
 from django.shortcuts import render, redirect
 
 
@@ -21,13 +22,14 @@ def loginProfile(req):
             password = form.cleaned_data["password"]
             profile = Profile.objects.get(phone=phone)
             if profile.user.check_password(password):
+                login(req, profile.user)
                 return redirect(reverse("account", args=[profile.user.id,]))
             return redirect(reverse("login"))
     else:
         form = ProfileLoginForm()
 
     return render(req, "login.html", {"form": form})
-def registerProfile(req):
+def registerProfile(req:HttpRequest):
     if req.method == "POST":
         form = ProfileRegisterForm(req.POST)
         if form.is_valid():
@@ -45,7 +47,6 @@ def registerProfile(req):
                 user.profile.phone = phone
                 user.profile.role = True
                 user.save()
-                    
             else:
                 user = User(username=name, first_name=surname, last_name=last_name)
                 user.set_password(password)
